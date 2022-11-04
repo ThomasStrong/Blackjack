0894000e0c65ddb25b5e18b091a404f46c9aacd8
Tom Strong <strng_thms@yahoo.com>
2022-11-03 20:45:09 -0400

    remove extra keystroke: 'press enter'

--- WinLose.cs
+++ WinLose.cs
@@ -22,35 +22,25 @@ namespace Blackjack
         public static void PlayerWins()
         {
             AnsiConsole.Markup($"[green]You Win!![/]\n");
-            AnsiConsole.Markup($"Press Enter to begin again.");
-            Console.ReadLine();
         }
 
         public static void PlayerBust()
         {
             AnsiConsole.Markup("[red]Bust! You lose.[/]\n");
-            AnsiConsole.Markup($"Press Enter to begin again.");
-            Console.ReadLine();
         }
 
         public static void PlayerLose()
         {
             AnsiConsole.Markup($"[red]You Lose...[/]\n");
-            AnsiConsole.Markup($"Press Enter to begin again.");
-            Console.ReadLine();
         }
 
         public static void Tie()
         {
             AnsiConsole.Markup($"TIE\n");
-            AnsiConsole.Markup($"Press Enter to begin again.");
-            Console.ReadLine();
         }
         public static void TieTwentyOne()
         {
             AnsiConsole.Markup($"[red]You TIE with 21![/]\n");
-            AnsiConsole.Markup($"Press Enter to begin again.");
-            Console.ReadLine();
         }
         public static void DealerHasBlackjack()
         {
@@ -61,8 +51,6 @@ namespace Blackjack
         public static void DealerBust()
         {
             AnsiConsole.Markup($"The Dealer busted!  [green]You Win!![/]\n");
-            AnsiConsole.Markup($"Press Enter to begin again.");
-            Console.ReadLine();
         }
 
     }
